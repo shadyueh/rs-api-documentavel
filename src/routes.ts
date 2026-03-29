e3dd35f655a93efa5a698ceb7797711074534d46
Held Grijo <held.grijo@gmail.com>
2026-03-28 22:49:48 -0400

    feat: atualiza requisição post users para conformidade com o padrão REST

--- src/routes.ts
+++ src/routes.ts
@@ -8,43 +8,51 @@ interface User {
     email: string
 }
 
-const users:User[] = []
+const users: User[] = []
 
-export async function routes(app:FastifyTypedInstance) {
- app.get('/users',{
-    schema:{
-        tags: ['users'],
-        description: 'List users',
-        response: {
-            200: z.array(
-                z.object({
-                id: z.string(),
+export async function routes(app: FastifyTypedInstance) {
+    app.get('/users', {
+        schema: {
+            tags: ['users'],
+            description: 'List users',
+            response: {
+                200: z.array(
+                    z.object({
+                        id: z.string(),
+                        name: z.string(),
+                        email: z.string()
+                    }))
+            }
+        }
+    }, () => { return users })
+
+    app.post('/users', {
+        schema: {
+            tags: ['users'],
+            description: 'Creates a new User',
+            body: z.object({
                 name: z.string(),
-                email: z.string()
-            }))
+                email: z.string().email()
+            }),
+            response: {
+                201: z.object({
+                    id: z.string().uuid(),
+                    name: z.string(),
+                    email: z.string().email()
+                }).describe('User created successfully')
+            }
         }
-    }
- }, ()=> {return users})
-
- app.post('/users', {
-    schema:{
-        tags: ['users'],
-        description: 'Creates a new User',
-        body: z.object({
-            name: z.string(),
-            email: z.string().email()
-        }),
-        response: {
-            201: z.null().describe('User created!')
+    }, async (request, reply) => {
+        const { name, email } = request.body
+        const newUser: User = {
+            id: randomUUID(),
+            name,
+            email
         }
-    }
- }, async (request, reply)=> {
-    const { name, email} = request.body
-    users.push({
-        id: randomUUID(),
-        name,
-        email
+        users.push(newUser)
+        return reply
+            .status(201)
+            .header('Location', `/users/${newUser.id}`)
+            .send(newUser)
     })
-    return reply.status(201).send()
-})
 }
